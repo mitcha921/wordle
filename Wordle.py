5f519b9d6324b60fc582bb26a04221e6cbd4f0cb
AngelinaWalton <95718621+AngelinaWalton@users.noreply.github.com>
2023-09-12 14:26:47 -0600

    select random word, display word, check if valid word

--- Wordle.py
+++ Wordle.py
@@ -12,10 +12,19 @@ from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
 def wordle():
 
-    def enter_action(s):
-        gw.show_message("You have to implement this method.")
-
     gw = WordleGWindow()
+
+    random_word = random.choice(FIVE_LETTER_WORDS)
+
+    for col in range(N_COLS):
+        gw.set_square_letter(0, col, random_word[col])
+
+    def enter_action(s):
+        if s in FIVE_LETTER_WORDS:
+            gw.show_message("'{}' is a possible word, great job!")
+        else:
+            gw.show_message("Sorry, '{}' is not in the word list.")
+
     gw.add_enter_listener(enter_action)
 
 # Startup code
