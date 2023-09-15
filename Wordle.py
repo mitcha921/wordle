b258bbf27e5e111e5fe52ca2be979d3b4fc6c332
AngelinaWalton <95718621+AngelinaWalton@users.noreply.github.com>
2023-09-15 11:20:29 -0600

    empty string does nothing, compare guess to answer, if statements to determine what color to color the box

--- Wordle.py
+++ Wordle.py
@@ -8,7 +8,7 @@ BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
 import random
 
 from WordleDictionary import FIVE_LETTER_WORDS
-from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
+from WordleGraphics import WordleGWindow, N_COLS, N_ROWS, CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR
 
 def wordle():
 
@@ -18,6 +18,9 @@ def wordle():
     print("the word is " + random_word)
 
     def enter_action(s):
+        if s.strip().lower() == "":
+            # If the input string is empty or contains only whitespace, do nothing
+            return
         if s.lower() in FIVE_LETTER_WORDS:
             if s.lower() == random_word.lower():
                 gw.show_message("You win! The word is " + s)
@@ -25,6 +28,27 @@ def wordle():
                 if gw.get_current_row() > 4 :
                     gw.show_message("Game over")
                 else: 
+                    correct_letter = []
+                    present_letter = []
+                    missing_letter = []
+
+                    for col in range(N_COLS):
+                        guess_letter = s[col]
+                        word_letter = random_word[col]
+
+                        if guess_letter.lower() == word_letter.lower():
+                            correct_letter.append(col)
+                        elif guess_letter.lower() in random_word.lower():
+                            present_letter.append(col)
+                        else:
+                            missing_letter.append(col)
+
+                    for col in correct_letter:
+                        gw.set_square_color(gw.get_current_row(), col, CORRECT_COLOR)
+                    for col in present_letter:
+                        gw.set_square_color(gw.get_current_row(), col, PRESENT_COLOR)
+                    for col in missing_letter:
+                        gw.set_square_color(gw.get_current_row(), col, MISSING_COLOR)
                     gw.show_message("Try Again")
                 gw.set_current_row(gw.get_current_row() + 1)
         else:
